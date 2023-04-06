4070937696d66511808311c202567158160dea5b
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-04-06 13:01:10 +0700

    use map biar sesuai soal ygy

--- responsi-5/GameKata/GameKata.java
+++ responsi-5/GameKata/GameKata.java
@@ -2,7 +2,15 @@ import java.util.*;
 
 public class GameKata {
   public static void run(String[] tito, String[] wiwid) {
-    // print all intersection using stream CASE-SENSITIVE (unique)
-    Arrays.stream(tito).filter(Arrays.asList(wiwid)::contains).distinct().forEach(System.out::println);
+    // use java api map to print the intersection of tito and wiwid (case sensitive and unique)
+    Map<String, Integer> map = new HashMap<>();
+    Arrays.stream(tito).forEach(x -> map.put(x,1));
+    Arrays.stream(wiwid).filter(map::containsKey).distinct().forEach(System.out::println);
+  }
+
+  public static void main(String[] args) {
+    String a[] = {"budi", "budi", "adalah", "budi"};
+    String b[] = {"budi", "adalah", "mapres"};
+    GameKata.run(a, b);
   }
 }
